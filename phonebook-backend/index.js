ea6a8a6595884ac2145f89f5b21ae8fe54488987
zane o <zaneolds@gmail.com>
2024-04-27 00:46:16 -0500

    3.8 configure morgan to show data of post request but only if it is a post request

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 
+morgan.token('content', request => JSON.stringify(request.body))
+
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
 
 let persons = [
     {
