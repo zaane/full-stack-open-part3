75e6a388a872d0799be66afd7f56ca2e11ac17e0
zane o <zaneolds@gmail.com>
2024-04-28 16:29:47 -0500

    3.17 handle put requests for updating numbers

--- index.js
+++ index.js
@@ -90,6 +90,21 @@ app.post('/api/persons', (request, response) => {
         })
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
+    const updatedPerson = {
+        name: body.name,
+        number: body.number
+    }
+
+    Person.findByIdAndUpdate(request.params.id, updatedPerson, { new: true } )
+        .then(returnedPerson => {
+            response.json(returnedPerson)
+        })
+        .catch(error => next(error))
+})
+
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
 }
